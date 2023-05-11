f4d1008dd29f6c0dfbebb38eb096ec8210747106
siusiusiublack1 <siusiusiublack@gmail.com>
2023-05-11 23:03:39 +0800

    20230511routerSetup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Outlet,
+} from "react-router-dom";
+
+import Navbar from "./components/Navbar"
+import Footer from "./components/Footer"
+import Home from "./page/Home"
+import Login from "./page/Login"
+import Register from "./page/Register"
+import Single from "./page/Single"
+import Write from "./page/Write"
+const Layout = () => {
+  return (
+    <div>
+     <Navbar/>
+     <Outlet/>
+     <Footer/>
+    </div>
+  )
+}
+
+const router = createBrowserRouter(
+[
+  {
+    path: "/",
+    element: <Layout/>,
+    children:[
+      {
+        path:"/",
+        element:<Home/>,
+      },
+      {
+        path:"/login",
+        element:<Login />,
+      },
+      {
+        path:"/register",
+        element:<Register/>,
+      },
+      {
+        path:"/post/:id",
+        element:<Single/>,
+      },
+      {
+        path:"/write",
+        element:<Write/>
+      }
+    ]
+  }
+]
+)
 
 function App() {
   return (
-    <div className="App">
-      Hello World!
+    <div >
+      <RouterProvider router={router} />
     </div>
   );
 }
+
+
 
 export default App;
