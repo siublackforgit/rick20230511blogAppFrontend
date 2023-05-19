1d64ec3745afda6b1fca29c4c5e2c03d54e60b15
siusiusiublack1 <siusiusiublack@gmail.com>
2023-05-19 11:54:50 +0800

    20230519authLoginRegisterSucceed

--- src/page/Login.jsx
+++ src/page/Login.jsx
@@ -1,14 +1,43 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import axios from 'axios'
+import React ,{useState}from 'react'
+import {Link, useNavigate} from 'react-router-dom'
 
 const Login = () => {
+  const [input, setInput] = useState({
+    username:"",
+    password:"",
+  })
+
+const [err,setErr] = useState(null)
+
+const handleChange = e =>{
+  setInput((prev)=>({
+    ...prev,
+    [e.target.name]:e.target.value
+  }))
+}
+
+const handleSubmit = async e =>{
+  e.preventDefault()
+  try {
+    await axios.post("http://localhost:8800/api/auth/login",input)
+    navigate("/")
+  }catch(err){
+    console.log(err)
+    setErr(err.response.data);
+  }
+}
+
+  const navigate = useNavigate();
   return (
     <div className="auth">
       <h1>Login</h1>
       <form action="">
-        <input type="text" placeholder='username' />
-        <input type="password" placeholder="password" />
-        <button>Login</button>
+        <input type="text" placeholder='username' name="username" onChange={handleChange}/>
+        <input type="password" placeholder="password" name="password" onChange={handleChange} />
+        <button onClick={handleSubmit}>Login</button>
+
+        {err && <p>{err}</p> }
         <span>Don't you have an account?<Link to="/register">Register</Link></span>
       </form>
     </div>
--- src/page/Register.jsx
+++ src/page/Register.jsx
@@ -1,13 +1,45 @@
 import React from 'react'
+import {useState} from "react"
+import { Link } from "react-router-dom"
+import axios from "axios"
 
 const Register = () => {
+  const [inputs,setInputs] = useState({
+    username:"",
+    email:"",
+    password:"",
+
+  })
+
+  const [err,setErr] = useState(null)
+
+  const handleChange =  e => {
+    setInputs((prev)=>({...prev,[e.target.name]:e.target.value}))
+  
+  }
+
+  const handleSubmit = async e=>{
+    e.preventDefault()
+    try{
+      const res = await axios.post("http://localhost:8800/api/auth/register",inputs)
+    }catch(err){
+      console.log(err)
+    setErr(err.response.data)
+    }
+  }
+
+  console.log(inputs)
   return (
     <div className="auth">
       <div>Register</div>
       <form action="">
-      
+        <input type="text" name="username" onChange={handleChange} placeholder='username'/>
+        <input type="email" name="email" placeholder='email' onChange={handleChange} />
+        <input type="password" name="password" placeholder='password' onChange={handleChange} />
+        <button onClick={handleSubmit}>Register</button>
+        {err && <p>{err}</p> }
+        <span>Do you have an account? <Link to="/login">Login</Link></span>
       </form>
-        
     </div>
   )
 }
